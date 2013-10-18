f3a752aeec221f90bd5bef92a9ae3b2d5739cb60
Adi Eyal <adi@burgercom.co.za>
2013-10-18 23:24:53 +0200

    Added fabric script for deployment

---
+++ fabdefs.sample.py
@@ -0,0 +1,9 @@
+from fabric import api
+
+"""
+This is a sample fabfile. Customise this fabfile to your settings.
+"""
+
+api.env.hosts = ["username@server:port"]
+code_dir = "/path/to/project/root
+env_dir = "/path/to/virtualenv/environment"
---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabdefs import *
+from fabric import api
+import os
+
+python = "%s/bin/python" % env_dir
+pip = "%s/bin/pip" % env_dir
+
+def deploy():
+    with api.cd(code_dir):
+        api.run("git pull origin master")
+        api.run("%s install -r %s/deploy/requirements.txt --quiet" % (pip, code_dir))
+        api.run("source %s/bin/activate; make setup" % env_dir)
+
+        #api.sudo("supervisorctl restart mpr")
